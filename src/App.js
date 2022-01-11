9fd28d7dc3b7c68abb88b93e14dbf13889326c42
MajidArthtech <majid.arthtech.s@gmail.com>
2022-01-11 22:19:50 +0530

    ShelaPages

--- src/App.js
+++ src/App.js
@@ -6,17 +6,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import HeadersPage from './Shela/FirstPage/Header/header';
 import FirstPage from './Shela/FirstPage/First';
+
+// import Second from './Shela/FirstPage/SecondPage/Second';
 import Bottom from './Shela/FirstPage/BottmPart/Bottom';
+// import Third from './Shela/FirstPage/ThirdPage/Third';
+// import Fourth from './Shela/FirstPage/FourthPage/Fourth';
+// import Five from './Shela/FirstPage/FivePage/Five';
+// import Six from './Shela/FirstPage/SixPage/Six';
+// import  First  from 'react-bootstrap/esm/PageItem';
+// import Seven from './Shela/FirstPage/SevenPage/Seven';
+
 function App() {
   return (
     <div className="App">
       <HeadersPage/>
-      < FirstPage/>
-      < FirstPage/>
-      < FirstPage/>
-      < FirstPage/>
+      <FirstPage/>
+      <FirstPage/>
+      <FirstPage/>
+      <FirstPage/>
       <Bottom/>
-                         
     </div>
   );
 }
